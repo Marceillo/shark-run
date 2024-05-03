f75a16b115af07c6cf7e6bbeec3336dc073b6465
Marceillo De Wet <marceillo@hotmail.com>
2024-05-03 12:26:42 +0000

    Update username text msg

--- run.py
+++ run.py
@@ -9,12 +9,15 @@ def username():
     """
     The function provides a option for the user to create there name. 
     This will use regular expresstion to validate ther user name using only letters with a length 1<15.
-    Will print error when false. 
+    Will print error when false and when True will print start msg. 
     """
     while True:
-        user_name = input("Ahoy there, matey! What might be your handle?\n ")
+        user_name = input("Ahoy there, matey! What might be your handle?\nA handle has letters only from 1 to 15 letters.\n ")
+              
         if 1 <= len(user_name) <=15 and re.fullmatch(r'^[A-Za-z]+$', user_name):
-            return user_name
+             print(f"{user_name}, ye scallywags! Lend us a hand to keep the briny deep safe,\n if ye please.Guess the words right,\n so the shark learns that the kayakers be no vittles for the likes of 'im!\n Should ye want to see the rules, press R, or should ye want to exit, press E, or press S to start.")
+             return user_name
+           
         else:
             print(f"{user_name}, ye scallywag! Yer handle should be naught but letters, from A to Z, both upper and lower,\n and be no shorter than a single letter and no longer than fifteen,\n with no other scurvy characters like numbers or symbols or the like!")
             break 

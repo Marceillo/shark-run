6dffd3cf4e2fbb4b81927eb9915bc576944a198d
Marceillo De Wet <marceillo@hotmail.com>
2024-05-15 12:46:58 +0000

    Copy code into pep8ci, clear errors and copy and past code back to run.

--- run.py
+++ run.py
@@ -1,6 +1,3 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
 import shark
 import random
 import re
@@ -12,44 +9,39 @@ def username():
     The function provides a option for the user to create username.
     This will use regular exprestion to validate the username.
     Using only letters with a length 1 to 15.
-    Will print a error when false and when True will print a message. 
+    Will print a error when false and when True will print a message.
     """
     while True:
-       
         print()
-        user_name = input("Ahoy there, matey! What might be your handle?\nA handle has letters only from 1 to 15 letters.\n ")
-              
-        if 1 <= len(user_name) <=15 and re.fullmatch(r'^[A-Za-z]+$', user_name):
+        user_name = input("Ahoy there, matey! What might be your handle?\n"
+                          "A handle has letters only from 1 to 15 letters.\n")
+        if 1 <= len(user_name) <= 15 and \
+           re.fullmatch(r'^[A-Za-z]+$', user_name):
             print(shark.correct_name(user_name))
-
             return user_name
-           
         else:
             print(shark.incorrect_name(user_name))
-            
 
-            
-            
+
 def gamerules(name):
     """
     To ask the user the user if they would like to see the rules.
-    If yes to print the rules to the consol. 
+    If yes to print the rules to the consol.
     """
     while True:
         game_rules = input(f"{name}! Should ye want to see the rules:\n"
-                            "Press Y too see rules.\n"
-                            "Press N not too see the rules and start the game.\n"
-                            "Press E too exit, savvy?\n")
+                           "Y too see rules.\n"
+                           "N not too see the rules and start the game.\n"
+                           "E too exit, savvy?\n")
         if re.fullmatch(r'[YyNnEe]', game_rules):
             game_rules = game_rules.lower()
         if game_rules == 'y':
             shark.clear_screen()
             shark.instructions()
-
             continue
-            
         elif game_rules == 'n':
-            print(f"{name} Alright, ye scallywags! Let's be shovin' off, shall we?")
+            print(f"{name} Alright, ye scallywags!\n"
+                  "Let's be shovin' off, shall we?")
             time.sleep(4)
             shark.clear_screen()
             break
@@ -57,7 +49,6 @@ def gamerules(name):
             shark.clear_screen()
             shark.exit_msg(name)
             exit()
-           
 
 
 def random_word():
@@ -67,12 +58,13 @@ def random_word():
     return random.choice(shark.words).lower()
 
 
-def guess_word_letter( name, guessed_letters):
+def guess_word_letter(name, guessed_letters):
     """
     For the user to guess the a single letter.
     This will be the main function.
     Provide information as to lives left and art graphics view.
-    User will guess the letters until completed or until life has been depleted.
+    User will guess the letters until completed,
+    or until life has been depleted.
     """
     word = random_word()
     letter_pattern = r'^[a-z]$'
@@ -81,9 +73,9 @@ def guess_word_letter( name, guessed_letters):
     guess_correct = []
     letter_word = ['_'] * len(word)
     lives = 6
-    letter_found = False 
-        
-    print(word) #nfor testing when needed
+    letter_found = False
+    # print(word)   for when testing is needed.
+
     while lives > 0:
         print(' '.join(letter_word))
         print(f"Ye have {lives} lives left, me hearty! ")
@@ -99,59 +91,52 @@ def guess_word_letter( name, guessed_letters):
             continue
 
         guessed_letters.add(guess_letter)
-        print(guessed_letters)     
-        
-        
+        print(guessed_letters)
 
-        # enumerate() method to iterate over char with i stores, index. 
         if guess_letter in word:
             print(f"Aye, that's correct: {guess_letter}")
             for i, char in enumerate(word):
                 if char == guess_letter:
                     letter_word[i] = guess_letter
-                    letter_found = True        
-                    
-                    
+                    letter_found = True
         else:
             print(f"Nay, matey: {guess_letter}")
             lives -= 1
-            letter_found = False 
-            
-           
-            
+            letter_found = False
+
         if "_" not in letter_word:
             print(' '.join(letter_word))
             shark.win_msg(word, name)
-            break    
+            break
+
     if lives == 0:
         shark.lost_msg(word, guessed_letters, name)
 
 
-def restart_shark( name, guessed_letters):
+def restart_shark(name, guessed_letters):
     """
     Provides an option to restart the game.
-    The user will press Y to start or N to exit. 
+    The user will press Y to start or N to exit.
     """
     while True:
         restart = input(f"Arrr, {name} would ye play again Y/N?\n")
         shark.clear_screen()
-        if re.fullmatch(r'^[yYnN]$',restart):
+        if re.fullmatch(r'^[yYnN]$', restart):
             restart = restart.lower()
             if restart == 'y':
                 print(f" Splendid {name}")
                 guessed_letters.clear()
                 word = random_word()
                 guess_word_letter(name, guessed_letters)
-               
             elif restart == 'n':
-                print(f"Arr, {name}! Should ye want to paddle again?\n Press run program.\n")
+                shark.exit_msg(name)
+                print(f"Arr! Should ye want to paddle again?\n"
+                      "Press run program.\n")
                 exit()
-                
-    else:
-        print("Foul value, Y or N only!")
-                
-  
-  
+        else:
+            print("Foul value, Y or N only!")
+
+
 def main():
     """
     To call all other functions in the game
@@ -161,8 +146,7 @@ def main():
     gamerules(name)
     guessed_letters = []
     guess_word_letter(name, guessed_letters)
-    restart_shark( name, guessed_letters)   
-    
+    restart_shark(name, guessed_letters)
+
+
 main()
-
-
